6e9463c8a23a2863eb20308a527bf4f121431a7c
Urmil <urmilkashyap845@gmail.com>
2026-09-24 08:34:20 +0530

    Add subtraction feature

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from calculator import add
+from calculator import add, subtract
 
 
 def main():
-    result = add(10, 20)
-    print("Calculator Result:", result)
+    addition_result = add(10, 20)
+    subtraction_result = subtract(20, 5)
+
+    print("Addition Result:", addition_result)
+    print("Subtraction Result:", subtraction_result)
 
 
 if __name__ == "__main__":
